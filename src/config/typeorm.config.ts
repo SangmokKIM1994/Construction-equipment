07862ba18d3ff5de5d6a96d67badd5b906669ba8
MOZZ\admin <yeong0319@naver.com>
2023-08-02 19:25:35 +0900

    refactor : 프리티어 적용

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,16 +1,16 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
+  type: "postgres",
   host: process.env.PGHOST,
   port: 5432,
   username: process.env.PGUSERNAME,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  entities: [__dirname + "/../**/*.entity.{js,ts}"],
   synchronize: true,
   ssl: {
     rejectUnauthorized: false,
