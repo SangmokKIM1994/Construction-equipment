1b0701bc6622ada69f3a56c2e021dffa21658081
MOZZ\admin <yeong0319@naver.com>
2023-11-16 18:13:44 +0900

    init : controller와 service에서 각각 service와 repository를 사용할수있게 인스턴스 생성

--- src/construction/construction.service.ts
+++ src/construction/construction.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ConstructionCompany } from "./construction.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class ConstructionService {}
+export class ConstructionService {
+  constructor(
+    @InjectRepository(ConstructionCompany)
+    private readonly constructionCompanyRepository: Repository<ConstructionCompany>
+  ) {}
+}
