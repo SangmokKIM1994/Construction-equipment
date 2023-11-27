4c9ea3e5a90ffbc01abc29bd0ef218e8775bacf2
MOZZ\admin <yeong0319@naver.com>
2023-11-27 18:04:41 +0900

    fix : controller에서 service , service에서 repository사용할수있게 세팅

--- src/equipment/equipment.service.ts
+++ src/equipment/equipment.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Equipment } from "./equipment.entity";
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
-export class EquipmentService {}
+export class EquipmentService {
+  constructor(
+    @InjectRepository(Equipment)
+    private readonly equipmentRepository: Repository<Equipment>
+  ) {}
+}
